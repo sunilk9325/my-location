22d17f0a8339c532b3b10beb3d38e6c40835fad0
Sunil Kumar <sunil.kumar@biz2credit.org>
2020-06-20 22:31:05 +0530

    Updates

---
+++ precache-manifest.989e2e9330d0fc953e0bc46c39afb51d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d80c564b0a181e4148129eb710c51d12",
+    "url": "/my-location/index.html"
+  },
+  {
+    "revision": "c8d057309215a69cb745",
+    "url": "/my-location/static/css/main.9cfad3ef.chunk.css"
+  },
+  {
+    "revision": "4f21a0459c4d578d4143",
+    "url": "/my-location/static/js/2.5fbe5466.chunk.js"
+  },
+  {
+    "revision": "1eed39b55c0bb7c26ec629a63f87aa2a",
+    "url": "/my-location/static/js/2.5fbe5466.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c8d057309215a69cb745",
+    "url": "/my-location/static/js/main.3f4656de.chunk.js"
+  },
+  {
+    "revision": "c3e7e758cf987b1af978",
+    "url": "/my-location/static/js/runtime-main.46033b99.js"
+  },
+  {
+    "revision": "0d704b704013154584d322fe6345aaea",
+    "url": "/my-location/static/media/location.0d704b70.png"
+  }
+]);
